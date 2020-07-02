37f2af0178fc3fbbcc76f5e71f6759e8e3a21219
Pointsz <pfvfake123@gmail.com>
2020-07-02 00:50:45 -0300

    updating files

--- cli.js
+++ cli.js
@@ -32,7 +32,7 @@ switch (argv._[0]) {
         const files = {
           bot: {
             room:
-              "Y29uc3Qgcm9vbSA9IEhCSW5pdCh7Cglyb29tTmFtZTogIk15IHJvb20iLAoJbWF4UGxheWVyczogMTYsCglub1BsYXllcjogdHJ1ZQp9KTsKCnJvb20uc2V0RGVmYXVsdFN0YWRpdW0oIkJpZyIpOwpyb29tLnNldFNjb3JlTGltaXQoNSk7CnJvb20uc2V0VGltZUxpbWl0KDApOwoKcm9vbS5vblBsYXllckpvaW4gPSAocGxheWVyKSA9PiByZXF1aXJlKCIuL2V2ZW50cy9vblBsYXllckpvaW4iKShwbGF5ZXIsIHJvb20pOwpyb29tLm9uUGxheWVyTGVhdmUgPSAocGxheWVyKSA9PiByZXF1aXJlKCIuL2V2ZW50cy9vblBsYXllckxlYXZlIikocGxheWVyLCByb29tKTs=",
+              "d2luZG93Lm9uSEJMb2FkZWQgPSAoKSA9PiB7CiAgY29uc3Qgcm9vbSA9IEhCSW5pdCh7CiAgICByb29tTmFtZTogIk15IHJvb20iLAogICAgbWF4UGxheWVyczogMTYsCiAgICBub1BsYXllcjogdHJ1ZSwKICAgIHRva2VuOiAidGhyMS5BQUFBQUY3OVVvLS02LXhfTXl3VklBLjdoNVRqdEpjNUR3IiwKICB9KTsKCiAgcm9vbS5zZXREZWZhdWx0U3RhZGl1bSgiQmlnIik7CiAgcm9vbS5zZXRTY29yZUxpbWl0KDUpOwogIHJvb20uc2V0VGltZUxpbWl0KDApOwoKICByb29tLm9uUGxheWVySm9pbiA9IChwbGF5ZXIpID0+CiAgICByZXF1aXJlKCIuL2V2ZW50cy9vblBsYXllckpvaW4iKShwbGF5ZXIsIHJvb20pOwogIHJvb20ub25QbGF5ZXJMZWF2ZSA9IChwbGF5ZXIpID0+CiAgICByZXF1aXJlKCIuL2V2ZW50cy9vblBsYXllckxlYXZlIikocGxheWVyLCByb29tKTsKICByb29tLm9uUm9vbUxpbmsgPSAobGluaykgPT4gY29uc29sZS5sb2cobGluayk7Cn07Cg==",
             utils: {
               updateAdmins:
                 "bW9kdWxlLmV4cG9ydHMgPSAocm9vbSkgPT4gewogIHZhciBwbGF5ZXJzID0gcm9vbS5nZXRQbGF5ZXJMaXN0KCk7CiAgaWYgKHBsYXllcnMubGVuZ3RoID09IDApIHJldHVybjsKICBpZiAocGxheWVycy5maW5kKChwbGF5ZXIpID0+IHBsYXllci5hZG1pbikgIT0gbnVsbCkgcmV0dXJuOwogIHJvb20uc2V0UGxheWVyQWRtaW4ocGxheWVyc1swXS5pZCwgdHJ1ZSk7Cn07Cg==",
